c8367c6bc8d5ba588b567174730395fc82b58d0b
DonSanoj <sanojaminda@gmail.com>
2024-02-16 15:10:45 +0530

    Create About section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import About from "@/components/layout/About";
 import Hero, { SectionHeaders } from "@/components/layout/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 }
